dab1132664d0836b325296fbf47feaaa73f9daf9
JonasParreno1994 <jonaspareno12345@gmail.com>
2023-08-25 21:56:37 +0800

    update

--- config.php
+++ config.php
@@ -6,10 +6,10 @@ date_default_timezone_set("Asia/Manila");
 
 // uncomment if you want to use database
 
-/* define( 'DBHOST', 'localhost' );
+define( 'DBHOST', 'localhost' );
 define( 'DBUSER', 'root' );
 define( 'DBPASS', '' );
-define( 'DBNAME', 'sample-db' ); */
+define( 'DBNAME', 'sample-db' );
 
 define( 'DEBUGGER', true );
 define( 'SITE_TITLE', 'Lorem PHP' );
